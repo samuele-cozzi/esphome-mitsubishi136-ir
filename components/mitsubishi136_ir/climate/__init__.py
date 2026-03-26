174207909ff01deaade759dba0a7b2ea860e79a0
samuele-cozzi <samuele.cozzi@gmail.com>
2026-03-26 13:58:50 +0000

    .

--- components/mitsubishi136_ir/climate/__init__.py
+++ components/mitsubishi136_ir/climate/__init__.py
@@ -1,5 +1,13 @@
-from esphome.components.mitsubishi136_ir.climate.mitsubishi136_ir_climate import Mitsubishi136IRClimate
+"""
+Mitsubishi136 IR climate.
+"""
+CONFIG_SCHEMA = esphome.configschema.CLIMATE_SCHEMA.extend({
+    esphome.configschema.Optional("ir_pin"): esphome.configschema.gpio_pin_schema,
+}).extend(esphome.configschema.UI_COLORSCHEME_CLIMATE)
 
-PLATFORMS = ["climate"]
-CONFIG_SCHEMA = PLATFORM_SCHEMA
-register_component("mitsubishi136_ir", Mitsubishi136IRClimate)
+async def to_code(config):
+    var = esphome.new_variable("Mitsubishi136IRClimate", "climate")
+    await esphome.register_parented(var)
+    if CONF_IR_PIN in config:
+        pin = await esphome.gpio_pin_expression(config[CONF_IR_PIN])
+        esphome.register_parented(var.set_ir_pin(pin))
