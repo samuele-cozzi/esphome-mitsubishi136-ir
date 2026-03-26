564ba4014616f444529cc7e3f5220b1df4ab2479
samuele-cozzi <samuele.cozzi@gmail.com>
2026-03-26 13:22:08 +0000

    first commit

---
+++ src/mitsubishi136_ir.h
@@ -0,0 +1,25 @@
+#include "esphome.h"
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+#include <ir_Mitsubishi.h>  // IRMitsubishi136
+
+namespace esphome {
+    namespace mitsubishi136_ir {
+
+        class Mitsubishi136IRClimate : public climate::Climate, public Component {
+        public:
+        void setup() override;
+        void dump_config() override;
+
+        climate::ClimateTraits traits() override;
+        void control(const climate::ClimateCall &call) override;
+
+        void set_ir_pin(uint8_t pin) { ir_pin_ = pin; }
+
+        protected:
+        uint8_t ir_pin_ = 4;
+        IRMitsubishi136 *ac_ = nullptr;
+        };
+
+    }  // namespace mitsubishi136_ir
+}  // namespace esphome
