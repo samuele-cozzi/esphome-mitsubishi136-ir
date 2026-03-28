99e41102f045f1b8e31db0d94355a2712d245459
Samuele Cozzi <samuele.cozzi@gmail.com>
2026-03-28 10:42:38 +0100

    .

--- components/mitsubishi136_ir/__init__.py
+++ components/mitsubishi136_ir/__init__.py
@@ -3,30 +3,7 @@ ESPHome component for Mitsubishi 136-bit IR climate control.
 """
 
 import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import climate
-from esphome.const import CONF_ID
 
 CODEOWNERS = ["@samuele-cozzi"]
-DEPENDENCIES = ["ir_transmitter"]
 
 mitsubishi136_ir_ns = cg.esphome_ns.namespace("mitsubishi136_ir")
-
-Mitsubishi136IRClimate = mitsubishi136_ir_ns.class_(
-    "Mitsubishi136IRClimate", climate.Climate, cg.Component
-)
-
-CONFIG_SCHEMA = cv.All(
-    climate.CLIMATE_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(Mitsubishi136IRClimate),
-        }
-    ).extend(cv.COMPONENT_SCHEMA)
-)
-
-
-async def to_code(config):
-    """Generate C++ code for the component."""
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await climate.register_climate(var, config)
---
+++ components/mitsubishi136_ir/climate/__init__.py
@@ -0,0 +1,27 @@
+"""
+ESPHome climate platform for Mitsubishi 136-bit IR climate control.
+"""
+
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import climate
+from esphome.const import CONF_ID
+
+from .. import mitsubishi136_ir_ns
+
+Mitsubishi136IRClimate = mitsubishi136_ir_ns.class_(
+    "Mitsubishi136IRClimate", climate.Climate, cg.Component
+)
+
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(Mitsubishi136IRClimate),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    """Generate C++ code for the climate platform."""
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await climate.register_climate(var, config)
