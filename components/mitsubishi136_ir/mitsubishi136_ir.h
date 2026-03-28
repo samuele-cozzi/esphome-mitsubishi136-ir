ecaab15bef64f350cbce4f65802ce1771645b457
Samuele Cozzi <samuele.cozzi@gmail.com>
2026-03-28 16:03:04 +0100

    .

--- components/mitsubishi136_ir/mitsubishi136_ir.h
+++ components/mitsubishi136_ir/mitsubishi136_ir.h
@@ -3,10 +3,6 @@
 #include "esphome/core/component.h"
 #include "esphome/components/climate/climate.h"
 
-#include <IRremoteESP8266.h>
-#include <IRsend.h>
-#include <ir_Mitsubishi.h>
-
 namespace esphome {
 namespace mitsubishi136_ir {
 
