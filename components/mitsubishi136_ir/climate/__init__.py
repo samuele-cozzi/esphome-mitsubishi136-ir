5cd767c0d247f616a378d32f7ed4d977defbcb68
Samuele Cozzi <samuele.cozzi@gmail.com>
2026-03-28 11:31:07 +0100

    .

--- components/mitsubishi136_ir/climate/__init__.py
+++ components/mitsubishi136_ir/climate/__init__.py
@@ -28,9 +28,6 @@ CONFIG_SCHEMA = cv.All(
 
 async def to_code(config):
     """Generate C++ code for the climate platform."""
-    # Register source files
-    cg.add_source_file("mitsubishi136_ir/mitsubishi136_ir.cpp")
-    
     var = cg.new_Pvariable(config[CONF_ID])
     await climate.register_climate(var, config)
     
